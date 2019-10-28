1e78f1d946463f874b2143152951be39057872c1
Anish <Anish.lohar.2022@student.lhschools.org>
2019-10-28 09:25:11 -0400

    added keyboard and ickNumber inside do and while

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -12,10 +12,15 @@ public class Main {
         do {
             System.out.println("would you like to win an prize?");
             System.out.println("if you do guess the right number you get to pick an prize.");
+            keyboard.nextInt();
+            // for( int = 0; i <20; i++) (
 
 
+            pickNumber = (int) (Math.random()*90)+3;
+            System.out.println(pickNumber);
 
 
+            
         }while
 
 
