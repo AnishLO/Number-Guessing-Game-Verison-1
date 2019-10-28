fa62b3fad668ee14fb161b6c5041904c17578d11
Anish <Anish.lohar.2022@student.lhschools.org>
2019-10-28 09:21:34 -0400

    initalized pick number

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
-        int picknumber;
+        int pickNumber;
+        pickNumber = (int) (Math.random()*90)+3;
+
 
     }
 
